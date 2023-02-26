0731cecd5ca8d75c0e89f8de3d9ae4d05ba6a531
Kevin Cogen <k.cogen@gmail.com>
2023-02-25 19:24:26 -0500

    timed string typing animation

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+
+const timedWriter = function(sentence, timing) {
+  let time = -timing
+  for (const char of sentence) {
+    time += timing
+    if (char === sentence[sentence.length -1]) {
+      setTimeout(() => {
+        process.stdout.write(`${char} \n`)
+      }, time)
+    } else if (char !== sentence[sentence.length -1]) {
+      setTimeout(() => {
+        process.stdout.write(char)
+      }, time) 
+    }
+  }
+};
+
+timedWriter("We want to type any sentence, bruv", 50)

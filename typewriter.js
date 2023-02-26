b20b8ea9ddd2b86649094c5abfe9a33c795c5718
Kevin Cogen <k.cogen@gmail.com>
2023-02-25 19:26:14 -0500

    timed string typing animation - linter

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,18 @@
 
 const timedWriter = function(sentence, timing) {
-  let time = -timing
+  let time = -timing;
   for (const char of sentence) {
-    time += timing
-    if (char === sentence[sentence.length -1]) {
+    time += timing;
+    if (char === sentence[sentence.length - 1]) {
       setTimeout(() => {
-        process.stdout.write(`${char} \n`)
-      }, time)
-    } else if (char !== sentence[sentence.length -1]) {
+        process.stdout.write(`${char} \n`);
+      }, time);
+    } else if (char !== sentence[sentence.length - 1]) {
       setTimeout(() => {
-        process.stdout.write(char)
-      }, time) 
+        process.stdout.write(char);
+      }, time);
     }
   }
 };
 
-timedWriter("We want to type any sentence, bruv", 50)
+timedWriter("We want to type any sentence, bruv", 50);
